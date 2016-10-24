a3cdcf66b4fd90f1f2b9dec871a4a2df9480ed32
Callan Morrison <callan.l.morrison@gmail.com>
2016-10-24 13:35:53 -0500

    updated number efficiency

--- server.js
+++ server.js
@@ -15,15 +15,23 @@ app.use(function(req, res, next) {
   next();
 });
 
-let collectiveCommits = 0;
+// let collectiveCommits = 0;
 
 // Gather all class Github Links provided in class data
-const classData = JSON.parse(fs.readFileSync('data/class-info.json', 'utf8'));
-const classGithubs = [];
-let {students} = classData;
-Object.keys(students).forEach(student => {
-  classGithubs.push(students[student].github)
-})
+
+
+let generateStudentGithubs = () => {
+  const classGithubs = [];
+  return new Promise(function(resolve, reject){
+    const classData = JSON.parse(fs.readFileSync('data/class-info.json', 'utf8'));
+    let {students} = classData;
+    Object.keys(students).forEach(student => {
+      classGithubs.push(students[student].github)
+    })
+    resolve(classGithubs)
+  })
+}
+
 
 // Gather list of periods in order to
 let periods = ["2016-01-01&to=2016-01-31", "2016-02-01&to=2016-02-28", "2016-03-01&to=2016-03-31", "2016-04-01&to=2016-04-30", "2016-05-01&to=2016-05-31", "2016-06-01&to=2016-06-30", "2016-07-01&to=2016-07-31", "2016-08-01&to=2016-08-31", "2016-09-01&to=2016-09-30", "2016-10-01&to=2016-10-31", "2016-11-01&to=2016-11-30", "2016-12-01&to=2016-12-31"]
@@ -34,27 +42,50 @@ currentMonth = currentMonth.getMonth();
 
 //Iterates through each of the classmembers and each given time period and scrapes the raw html data from github
 const commitGenerate = new Promise(function(resolve, reject){
-  const expectedIterateCount = (currentMonth + 1) * (classGithubs.length) - 1
-  let iterateCount = 0;
-    classGithubs.forEach((github)=>{
-      for (var i = 0; i <= currentMonth; i++){
-        fetch(`${github}?tab=overview&from=${periods[i]}`)
+  generateStudentGithubs().then((classGithubs)=> {
+    const expectedIterateCount = (currentMonth + 1) * (classGithubs.length) - 1
+    let iterateCount = 0;
+      classGithubs.forEach((github)=>{
+        for (var i = 0; i <= currentMonth; i++){
+          fetch(`${github}?tab=overview&from=${periods[i]}`)
+            .then(function(res) {
+                return res.text();
+            }).then(function(body) {
+               const $ = cheerio.load(body);
+               collectiveCommits += commitNumberParser($($("#js-contribution-activity").find("h4.m-0")[0]).html().toString());
+               iterateCount++
+               if (expectedIterateCount === iterateCount) {resolve({"collective commits": collectiveCommits, "students": classGithubs.length})}
+            });
+          }
+      });
+  })
+})
+
+const EfficientCommitGenerate = () => {
+  return new Promise((resolve, reject) => {
+    let collectiveCommits = 0;
+    generateStudentGithubs().then(classGithubs => {
+      let iterateCount = 0;
+      let expectedIterateCount = classGithubs.length
+      classGithubs.forEach((github)=>{
+        fetch(`${github}`)
           .then(function(res) {
               return res.text();
           }).then(function(body) {
              const $ = cheerio.load(body);
-             collectiveCommits += commitNumberParser($($("#js-contribution-activity").find("h4.m-0")[0]).html().toString());
+             EfficientCommitNumberParser($($($(".js-contribution-graph")[0]).find("h2")[0]).html().toString()).then((number) => {collectiveCommits += number});
              iterateCount++
-             if (expectedIterateCount === iterateCount) {resolve(collectiveCommits)}
+             if (expectedIterateCount === iterateCount) {resolve({"collective commits": collectiveCommits, "students": classGithubs.length})}
           });
-        }
-    });
-})
+      })
+    })
+  })
+}
 
 //Uses regex to match the number of commits in that given time period
 const regex = /Created\n*\s*([0-9]+)\n*\s*commit/gi;
 const commitNumberParser = (string) => {
-  let result = regex.exec(string)
+  let result = regex.exec(string);
   let returnNumber = 0;
   if (result) {
     returnNumber = Number(result[1])
@@ -62,10 +93,24 @@ const commitNumberParser = (string) => {
   return returnNumber
 }
 
+//Uses optimized regex to match the number of commits over the last year
+const EfficientRegex = /\n*\s*([0-9]+\,?[0-9]+)\n*\s*contribution/;
+const EfficientCommitNumberParser = (string) => {
+  return new Promise(function(resolve,reject){
+    let result = EfficientRegex.exec(string)
+    let returnNumber = 0;
+    if (result) {
+      tempNum = result[1].split(",").join("");
+      returnNumber = Number(tempNum)
+    }
+    resolve(returnNumber)
+  })
+}
+
+
 //ROUTE FOR API CALL
 app.get('/api/class-commits', (req, res, err) => {
-  console.log(req)
-  commitGenerate.then((com)=> res.json({"commits": com}))
+  EfficientCommitGenerate().then((com)=> res.json({"commits": com}))
 })
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
